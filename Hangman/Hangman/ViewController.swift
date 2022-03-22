f3dfd8436a1bab494b0cf540b7e431ad0dfe40d9
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-03-22 13:06:09 -0500

    Add score, answer, count labels.

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,6 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var wordCountLabel: UILabel!
+    var answerLabel: UILabel!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        wordCountLabel = UILabel()
+        wordCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordCountLabel.font = UIFont.systemFont(ofSize: 12)
+        wordCountLabel.text = "COUNT"
+        wordCountLabel.numberOfLines = 0
+        view.addSubview(wordCountLabel)
+        
+        answerLabel = UILabel()
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        answerLabel.font = UIFont.systemFont(ofSize: 24)
+        answerLabel.text = "ANSWERS"
+        answerLabel.numberOfLines = 0
+        view.addSubview(answerLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            answerLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 40),
+            answerLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            
+            wordCountLabel.topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 20),
+            wordCountLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+            
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
