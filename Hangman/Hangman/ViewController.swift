1b9abe61dd4153810c2bf54f8b863601685d2b88
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-03-22 16:34:49 -0500

    Add letter button alphabets.

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     var answerLabel: UILabel!
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
+    let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
     override func loadView() {
         view = UIView()
@@ -58,13 +59,45 @@ class ViewController: UIViewController {
             buttonsView.heightAnchor.constraint(equalToConstant: 320),
             buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             buttonsView.topAnchor.constraint(equalTo: wordCountLabel.bottomAnchor, constant: 20),
-            buttonsView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -20)
+            buttonsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
+        
+        let width = 75
+        let height = 50
+
+        var column = 0
+        var row = 0
+
+        for i in 0..<letters.count {
+            let letterButton = UIButton(type: .system)
+            letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+            letterButton.setTitle("\(letters[i].uppercased())", for: .normal)
+            letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+
+            if i % 7 == 0 {
+                column = 0
+                row += 1
+            }
+
+            let frame = CGRect(x: column * height, y: row * height, width: width, height: height)
+            letterButton.frame = frame
+
+            buttonsView.addSubview(letterButton)
+            letterButtons.append(letterButton)
+
+            column += 1
+
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func letterTapped() {
+        
     }
 
 
