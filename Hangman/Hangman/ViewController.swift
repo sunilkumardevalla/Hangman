0ea0583bcef5124cbe5482901cc2c927e98622a1
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-03-22 15:21:40 -0500

    Add buttons view.

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -25,29 +25,40 @@ class ViewController: UIViewController {
         
         wordCountLabel = UILabel()
         wordCountLabel.translatesAutoresizingMaskIntoConstraints = false
-        wordCountLabel.font = UIFont.systemFont(ofSize: 12)
+        wordCountLabel.font = UIFont.systemFont(ofSize: 15)
         wordCountLabel.text = "COUNT"
         wordCountLabel.numberOfLines = 0
+        wordCountLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         view.addSubview(wordCountLabel)
         
         answerLabel = UILabel()
         answerLabel.translatesAutoresizingMaskIntoConstraints = false
-        answerLabel.font = UIFont.systemFont(ofSize: 24)
+        answerLabel.font = UIFont.systemFont(ofSize: 28)
         answerLabel.text = "ANSWERS"
         answerLabel.numberOfLines = 0
+        answerLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         view.addSubview(answerLabel)
         
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
+        
         NSLayoutConstraint.activate([
-            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
             scoreLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             
-            answerLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 40),
+            answerLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 100),
             answerLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             
             
-            wordCountLabel.topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 20),
-            wordCountLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+            wordCountLabel.topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 10),
+            wordCountLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             
+            buttonsView.widthAnchor.constraint(equalToConstant: 750),
+            buttonsView.heightAnchor.constraint(equalToConstant: 320),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: wordCountLabel.bottomAnchor, constant: 20),
+            buttonsView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -20)
         ])
     }
 
